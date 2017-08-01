137ab3545887ef55e731480a355642b98a99dbe8
Dmitriy Oboldin <oboldin_db@sima-land.ru>
2017-08-01 13:41:15 +0500

    Change Task model and add TaskSearch model

--- common/models/Task.php
+++ common/models/Task.php
@@ -16,21 +16,41 @@ use common\fixtures\TaskFixture;
  * @property integer $user_id
  */
 
+
 class Task extends ActiveRecord
 {
 
 
     public function rules(){
         return [
-
             [['begin_at', 'end_at', 'description', 'user_id'], 'required'],
-            ['id', 'integer'],
-            [['begin_at', 'end_at'], 'string'],
-            ['is_complete', 'boolean'],
-            ['description', 'string', 'length' => [3, 100]],
-            [['begin_at', 'end_at', 'description'], 'safe'],
+            [['begin_at', 'end_at'], 'safe'],
+            [['is_complete'], 'boolean'],
+            [['user_id'], 'integer'],
+            [['description'], 'string',  'length' => [3, 100]],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
 
         ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'begin_at' => 'Начало',
+            'end_at' => 'Конец',
+            'is_complete' => 'Завершено',
+            'description' => 'Описание',
+            'user_id' => 'ID пользователя',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
     }
 
 }
